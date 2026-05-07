87a2b09c5f7223a7451239bd48c7cd0b3986df26
SChinmaya15 <samchinmaya15@gmail.com>
2026-05-07 22:48:53 +0530

    pushed the swagger changes

--- PiiScanner.API/Program.cs
+++ PiiScanner.API/Program.cs
@@ -58,11 +58,9 @@ builder.Services.AddSwaggerGen(c =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
