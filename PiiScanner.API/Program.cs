8eaaf369f3fd3e996bb995cf34d824b1acb3b5fe
imbhavneetkumar <98938895+imbhavneetkumar@users.noreply.github.com>
2026-05-08 14:07:53 +0530

    Modify root endpoint and remove Swagger configuration
    Updated the root endpoint response message and removed Swagger setup.

--- PiiScanner.API/Program.cs
+++ PiiScanner.API/Program.cs
@@ -1,34 +1,7 @@
-using Microsoft.OpenApi.Models;
-
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers();
-
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "PII Scanner API",
-        Version = "v1"
-    });
-});
 
 var app = builder.Build();
 
-// TEST ROOT
-app.MapGet("/", () => "API RUNNING SUCCESSFULLY");
-
-// Swagger
-app.UseSwagger();
-
-app.UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "PII Scanner API V1");
-});
-
-// Controllers
-app.MapControllers();
+app.MapGet("/", () => "API WORKING");
 
 app.Run();
